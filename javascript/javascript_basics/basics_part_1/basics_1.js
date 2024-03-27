745aeb5a1c4c228646645bad39061d0270e990b4
Akash Mishra <162899600+Akashmishra0601@users.noreply.github.com>
2024-03-27 15:55:13 +0000

    Pending changes exported from your codespace

---
+++ javascript/javascript_basics/basics_part_1/basics_1.js
@@ -0,0 +1,22 @@
+console.log("Hello Everyone");
+
+
+
+// var in func , table , alert() , 
+
+// Datatype and datatype conversion , Number(score),
+
+let var_1 = "hello"
+let n = null
+const var_2 = 12
+var var_3 = true
+const un = undefined
+
+
+console.log(var_1 , var_2 , var_3 , n , un)
+console.table([var_1 , var_2 , var_3 , n , un])
+console.log(typeof(var_1))
+console.log(typeof(var_2))
+console.log(typeof(var_3))
+console.log(typeof(un))
+console.log(typeof(n))
